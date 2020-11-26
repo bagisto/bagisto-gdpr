37ebfcc90f790bae42b2781db0fa21d539cc017b
Akhtar Khan <akhtar.khan671@webkul.com>
2020-11-26 20:18:30 +0530

    icon added on customers end

--- src/Config/menu.php
+++ src/Config/menu.php
@@ -5,6 +5,7 @@ return [
         'key' => 'account.gdpr',
         'name' => 'gdpr::app.shop.customer.menu-name',
         'route' => 'gdpr.customers.allgdpr',
-        'sort' => 8
+        'sort' => 8,
+        'icon-class' => 'fa fa-asterisk'
     ]
 ];
