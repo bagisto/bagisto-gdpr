72306ed67e3c4573b1b1696b131420919d6ecd87
Glenn Hermans <glhermans@gmail.com>
2021-06-14 21:47:26 +0200

    Added install command

---
+++ src/Console/Commands/Install.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Webkul\GDPR\Console\Commands;
+
+use Illuminate\Console\Command;
+use Artisan;
+
+class Install extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'gdpr:install';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'This will prepare the GDPR package';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        Artisan::call('migrate');
+        Artisan::call('db:seed --class=Webkul\\GDPR\\Database\\Seeders\\GdprTableSeeder');
+        Artisan::call('route:optimize');
+        Artisan::call('vendor:publish --provider="Bagisto\GDPR\Providers\GDPRServiceProvider" --force');
+    }
+}
